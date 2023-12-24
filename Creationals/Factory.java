d7ea23aa261255a73c496f99ebb7047f7a3c333d
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2023-12-24 22:10:51 +0530

    Create Factory.java

---
+++ Creationals/Factory.java
@@ -0,0 +1,16 @@
+/*
+//@Author -- Hitesh
+
+
+Factory DP
+
+So, let's talk about this factory magic! Picture it as your personal credit helper.
+When you say, "I need my credit report," the factory gets into action. It knows about TransUnion and Experian, like your expert buddies for credit stuff.
+The factory figures out which buddy to call, gets your credit info, and hands it over to you.
+It's like having a clever friend who knows just where to go for what you need. Simple, smooth, and no stress involved! 
+
+
+
+*/
+
+
